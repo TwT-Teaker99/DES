fff6e7e6b59bdee828248e69a687f251a655f538
TwT-Teaker99 <49748759+TwT-Teaker99@users.noreply.github.com>
2020-09-17 15:27:54 +0700

    Load

---
+++ DES/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace DES
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxReturn = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PlainText = new System.Windows.Forms.Label();
+            this.textBoxText = new System.Windows.Forms.TextBox();
+            this.btnCrypt = new System.Windows.Forms.Button();
+            this.textBoxKEY = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDecrypt);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxReturn);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.PlainText);
+            this.groupBox1.Controls.Add(this.textBoxText);
+            this.groupBox1.Controls.Add(this.btnCrypt);
+            this.groupBox1.Controls.Add(this.textBoxKEY);
+            this.groupBox1.Location = new System.Drawing.Point(4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(793, 443);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "DES";
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnDecrypt.Location = new System.Drawing.Point(445, 359);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(175, 46);
+            this.btnDecrypt.TabIndex = 4;
+            this.btnDecrypt.Text = "Giải mã";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(13, 262);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "CipherText";
+            // 
+            // textBoxReturn
+            // 
+            this.textBoxReturn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.textBoxReturn.Location = new System.Drawing.Point(131, 201);
+            this.textBoxReturn.Multiline = true;
+            this.textBoxReturn.Name = "textBoxReturn";
+            this.textBoxReturn.Size = new System.Drawing.Size(592, 119);
+            this.textBoxReturn.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(13, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Key";
+            // 
+            // PlainText
+            // 
+            this.PlainText.AutoSize = true;
+            this.PlainText.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.PlainText.Location = new System.Drawing.Point(13, 63);
+            this.PlainText.Name = "PlainText";
+            this.PlainText.Size = new System.Drawing.Size(87, 20);
+            this.PlainText.TabIndex = 9;
+            this.PlainText.Text = "PlainText";
+            // 
+            // textBoxText
+            // 
+            this.textBoxText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.textBoxText.Location = new System.Drawing.Point(131, 21);
+            this.textBoxText.Multiline = true;
+            this.textBoxText.Name = "textBoxText";
+            this.textBoxText.Size = new System.Drawing.Size(592, 104);
+            this.textBoxText.TabIndex = 1;
+            // 
+            // btnCrypt
+            // 
+            this.btnCrypt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnCrypt.Location = new System.Drawing.Point(179, 362);
+            this.btnCrypt.Name = "btnCrypt";
+            this.btnCrypt.Size = new System.Drawing.Size(175, 43);
+            this.btnCrypt.TabIndex = 3;
+            this.btnCrypt.Text = "Mã hoá";
+            this.btnCrypt.UseVisualStyleBackColor = true;
+            this.btnCrypt.Click += new System.EventHandler(this.btnCrypt_Click);
+            // 
+            // textBoxKEY
+            // 
+            this.textBoxKEY.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.textBoxKEY.Location = new System.Drawing.Point(131, 142);
+            this.textBoxKEY.Multiline = true;
+            this.textBoxKEY.Name = "textBoxKEY";
+            this.textBoxKEY.Size = new System.Drawing.Size(592, 35);
+            this.textBoxKEY.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "DES";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxReturn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label PlainText;
+        private System.Windows.Forms.TextBox textBoxText;
+        private System.Windows.Forms.Button btnCrypt;
+        private System.Windows.Forms.TextBox textBoxKEY;
+    }
+}
+
---
+++ DES/Form1.cs
@@ -0,0 +1,60 @@
+﻿using DES.Function;
+using DES.Object;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DES
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBoxKEY.Text = "0123456789ABCDE1";//khoá kiểu hex 64bit
+        }
+
+        int MaHoaOrGiaMa = 1;//chế độ(=1:mã hoá, =-1:giải mã)
+        Khoa key;
+        Des des;
+        void MaHoa()
+        {
+            key = new Khoa(textBoxKEY.Text);
+            if (key.KiemTraKhoa())
+            {
+                string text = textBoxText.Text;
+                des = new Des();
+                string cipher = des.MaHoa(text, key, chose: MaHoaOrGiaMa);
+                textBoxReturn.Text = cipher;
+            }
+            else
+            {
+                MessageBox.Show("Key không hợp lệ!");
+            }    
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)//giải mã
+        {
+            MaHoaOrGiaMa = -1;
+            MaHoa();
+        }
+
+        private void btnCrypt_Click(object sender, EventArgs e)//mã hoá
+        {
+            MaHoaOrGiaMa = 1;
+            MaHoa();
+        }
+
+    }
+}
